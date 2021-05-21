6a396438ebc14ef46a98e00788385bac1dbe2a0b
jeanmarcjulien <julienjeanmarc@gmail.com>
2021-05-21 16:20:40 -0500

    Update CalculatorTestCase.java

--- src/test/java/com/galvanize/CalculatorTestCase.java
+++ src/test/java/com/galvanize/CalculatorTestCase.java
@@ -17,7 +17,7 @@ class CalculatorTestCase {
         calculator = new Calculator();
 
         //execution
-        int expected=1;
+        int expected=0;
         int actual = calculator.getCurrentState();
 
         //assertion
